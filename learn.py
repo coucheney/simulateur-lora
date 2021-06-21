0be633b370fbd996f07055b906ea070ec3f937bf
plicence <32228666+plicence@users.noreply.github.com>
2021-06-21 11:49:06 +0200

    Update des fonctions de learning

--- learn.py
+++ learn.py
@@ -4,11 +4,16 @@ import random
 La node ne change pas les paramètre de la Node
 Sert également de classe de base pour la hiérachie des object qui permettent le choix des paramètres
 """
+
+
 class Static:
     def chooseParameter(self, power: int, SF: int, lostPacket: bool, validCombination: list, nbSend: int):
         return SF, power
 
+
 """Si collision, les paramètre sont tirés aléatoirement dans les paramètres valides"""
+
+
 class RandChoise(Static):
     def chooseParameter(self, power, SF, lostPacket, validCombination, nbSend):
         if lostPacket:
@@ -28,7 +33,7 @@ class RandChoise(Static):
             if SF > 7:
                 SF -= 1
             elif power > 2:
-               power -= 1
+                power -= 1
         return SF, power
 
 
@@ -36,7 +41,6 @@ import numpy as np
 import math
 import random
 from scipy.stats import beta
-from scipy import special as sp
 
 
 class UCB1:
@@ -47,16 +51,17 @@ class UCB1:
 
     def select_arm(self, lr):
         """ Selectionne le bras avec la valeur de l'estimateur la plus haute"""
+        # Parcours des choix qui n'ont pas encore été visités
         for arm in range(self.n_arms):
             if self.counts[arm] == 0:
                 return arm
-
         ucb_values = [0.0 for arm in range(self.n_arms)]
         total_counts = sum(self.counts)
+        # Calcul des valeurs d'UCB1 pour les différents choix
         for arm in range(self.n_arms):
             bonus = math.sqrt((math.log(total_counts)) / float(self.counts[arm]))
             ucb_values[arm] = self.values[arm] + 0.15 * bonus
-
+        # On choisit celui avec la valeur la plus élevée
         value_max = max(ucb_values)
         return ucb_values.index(value_max)
 
@@ -68,6 +73,7 @@ class UCB1:
         self.values[chosen_arm] = new_value
 
 
+# Version d'Exp3 trouvée sur internet et qui donne des résultats différents de ma version
 class Exp3:
     def __init__(self, counts=[], values=[], n_arms=0):
 
@@ -141,80 +147,45 @@ class ThompsonSampling():
     def __init__(self, counts=[], values=[], n_arms=0):
         self.a = [1 for arm in range(n_arms)]
         self.b = [1 for arm in range(n_arms)]
-        self.counts = [0 for i in range(n_arms)]
-        self.index = 0
         self.n_arms = n_arms
-        self.previous_reward = 0
         self.modified = True
         self.all_draws = [0 for i in range(n_arms)]
-        self.all_means = []
-        self.counter = 10
-        self.old_arm = 0
 
-    # Thompson Sampling selection of arm for each round
     def select_arm(self, useless):
-        # n_arms = len(self.counts)
-
+        """Thompson Sampling : sélection de bras"""
         if self.modified:
-            # Pair up all beta params of a and b for each arm
+            # On groupe toutes les paires de a et b et on tire une valeur selon distribution béta
             beta_params = zip(self.a, self.b)
-            # Perform random draw for all arms based on their params (a,b)
-           # x = random.random()
-            """for i in beta_params:
-                self.all_draws[i] = beta.rvs(i[0], i[1], size=1) #(self.a[i] + self.b[i]) + np.random.normal(0, 0.001) #self.all_draws[i] = ((x**(self.a[i]-1)) * ((1-x)**(self.b[i]-1))) / sp.beta(self.a[i], self.b[i])  #beta.rvs(self.a[i], self.b[i], size=1) #self.a[i] /
-            """
-            all_draws = []
-                # self.all_means.append(self.all_draws[i] - beta.rvs(self.a[i], self.b[i], size=1000).mean())
             all_draws = [beta.rvs(i[0], i[1], size=1) for i in beta_params]
             self.all_draws = all_draws
-            # self.index = all_draws.index(max(all_draws))
-            # return index of arm with the highest draw
-        self.old_arm = np.argmax(self.all_draws)
-        return self.old_arm  # self.all_draws.index((max(self.all_draws)))
+        return np.argmax(self.all_draws)
 
-        """else:
-            return random.randint(0, self.n_arms - 1)
-"""
-
-    def estimate(self, x, gamma):
-        return x
-
-    def delta(self, x):
-        return x - self.previous_reward
-
-    # Choose to update chosen arm and reward
-    def update(self, chosen_arm, reward, epsilon=0.5):
-        # update counts pulled for chosen arm
-
-        self.counts[chosen_arm] = self.counts[chosen_arm] + 1
-        n = self.counts[chosen_arm]
-        # Update average/mean value/reward for chosen arm
-        # value = self.values[chosen_arm]
-        # new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
-        # self.values[chosen_arm] = new_value
-        # Update a and b
-        # a is based on total counts of rewards of arm
-        epsilon = 0.5 #1 / (0.0000005 * n)
+    def update(self, chosen_arm, reward):
+        """Choix du bras à mettre à jour"""
+        # epsilon correpsond à la probabilité de mettre à jour a et b (basé sur Adaptative rate Thomspon Sampling de Basu et Ghosh)
+        epsilon = 0.5
         omega = random.random()
         if omega <= epsilon:
-            self.a[chosen_arm] = self.a[chosen_arm] + self.estimate(reward, 1)
-            # b is based on total counts of failed rewards on arm
-            self.b[chosen_arm] = self.b[chosen_arm] + self.estimate(1 - reward, 1)
+            # a est basé sur les succès
+            self.a[chosen_arm] = self.a[chosen_arm] + reward
+            # b est basé sur les échecs
+            self.b[chosen_arm] = self.b[chosen_arm] + (1 - reward)
             self.modified = True
         else:
             self.modified = False
-        self.previous_reward = reward
 
 
 class AB:
-    def __init__(self, k=250, n_arms=0):
+    def __init__(self, k=0, n_arms=0):
+        # bras actuel
         self.index = 0
+        # taille du batch
         self.k = k
         self.utilities = [0.0 for col in range(n_arms)]
         self.n_arms = n_arms
 
     def select_arm(self, lr):
-        """ Selectionne le bras avec la valeur de l'estimateur la plus haute"""
+        """ Selectionne le bras avec la valeur de recommandation la plus haute"""
         old_index = self.index
         self.index = self.index + 1
         if old_index < self.k:
@@ -226,28 +197,22 @@ class AB:
         self.utilities[chosen_arm] += reward
 
 
-class EXP3BIS:
-
-    def __init__(self, k=250, n_arms=0.0):
-        self.Bandit = Bt.Exp3(nbArms=n_arms, gamma=0.2)
-
-    def select_arm(self, lr, rank=1):
-        return self.Bandit.choiceWithRank(rank)
-
-    def update(self, chosen_arm, reward):
-        self.Bandit.getReward(chosen_arm, reward)
-
-
 class MyExp3:
     def __init__(self, gamma=0.1, n_arms=0.0):
+        # poids attribués par bras
         self.weights = [1.0 for i in range(0, n_arms)]
+        # probabilité d'utiliser
         self.proba = [1.0 for i in range(0, n_arms)]
+        # nombre de bras
         self.n_arms = n_arms
+        # paramètre à régler
         self.gamma = gamma
 
     def select_arm(self, lr=0):
+        # Mise à jour de la probabilité des bras
         for i in range(0, self.n_arms):
             self.proba[i] = (1 - self.gamma) * (self.weights[i] / np.sum(self.weights)) + self.gamma / self.n_arms
+        # Sélection d'un bras selon la distribution
         tmpSum = 0
         randomVal = random.random()
         for i in range(self.n_arms):
@@ -259,41 +224,6 @@ class MyExp3:
     def update(self, chosen_arm, reward):
         estimated = reward / self.proba[chosen_arm]
         self.weights[chosen_arm] = self.weights[chosen_arm] * np.exp((self.gamma / self.n_arms) * estimated)
-
-class GittinsIndex:
-    def __init__(self, epsilon=1, n_arms=0.0):
-        self.armReward = [0.0 for i in range(0, n_arms)]
-        self.counts = [0.0 for i in range(0, n_arms)]
-        self.valueExpectation = [0.0 for i in range(0, n_arms)]
-        self.n_arms = n_arms
-        self.epsilon = epsilon
-        self.l = 0
-        self.u = n_arms - 1
-        self.reward_sum = 0
-        self.n
-
-
-    def return_bounds(self):
-        return self.l, self.u
-
-    def calculateV(self, arm, lambd, reward):
-        gamma = 0.1
-        self.valueExpectation[arm] += 0.5 * reward
-        self.n_arms[arm] += 1
-        return max((self.armReward[arm]/self.counts[arm]) - lambd + gamma * self.valueExpectation[arm], 0)
-
-
-    def udpate(self, safe, risky, reward):
-        if self.u - self.l > self.epsilon:
-            lambd = (self.l + self.u)/2
-            v = self.calculateV(risky, lambd, reward)
-            self.valueExpectation[risky] = v
-            if v > 0:
-                self.l = lambd
-            else:
-                self.u = lambd
-        return self.l, self.u
-
 
 
 
